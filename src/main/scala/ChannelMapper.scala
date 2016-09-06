8df103f48553d018b2fed320be4e4496bf6d5ac3
Howard Mao <zhehao.mao@gmail.com>
2016-09-06 16:10:19 -0700

    add a block for mapping and unmapping channel addresses

---
+++ src/main/scala/ChannelMapper.scala
@@ -0,0 +1,72 @@
+package testchipip
+
+import Chisel._
+import uncore.tilelink._
+import cde.Parameters
+
+/**
+ * Assumes every channel has distinct addresses strided per block.
+ * Cuts down the addresses on the output so that all the addresses for each
+ * channel are contiguous.
+ */
+class ChannelAddressMapper(n: Int)(implicit p: Parameters) extends Module {
+  val io = new Bundle {
+    val in  = Vec(n, new ClientUncachedTileLinkIO).flip
+    val out = Vec(n, new ClientUncachedTileLinkIO)
+  }
+
+  if (n <= 1) {
+    io.out <> io.in
+  } else {
+    val idBits = log2Up(n)
+
+    for (((in, out), i) <- io.in.zip(io.out).zipWithIndex) {
+      val in_addr_block = in.acquire.bits.addr_block
+      out.acquire.valid := in.acquire.valid
+      in.acquire.ready := out.acquire.ready
+      out.acquire.bits := in.acquire.bits
+      out.acquire.bits.addr_block := in_addr_block >> UInt(idBits)
+      in.grant <> out.grant
+    }
+  }
+}
+
+/**
+ * Does the opposite of the mapper. Takes contiguous addresses on the input
+ * and transforms them to be distinct per channel on the output.
+ */
+class ChannelAddressUnmapper(n: Int)(implicit p: Parameters) extends Module {
+  val io = new Bundle {
+    val in =  Vec(n, new ClientUncachedTileLinkIO).flip
+    val out = Vec(n, new ClientUncachedTileLinkIO)
+  }
+
+  val idBits = log2Up(n)
+
+  if (n <= 1) {
+    io.out <> io.in
+  } else {
+    for (((in, out), i) <- io.in.zip(io.out).zipWithIndex) {
+      val in_addr_block = in.acquire.bits.addr_block
+      out.acquire.valid := in.acquire.valid
+      in.acquire.ready := out.acquire.ready
+      out.acquire.bits := in.acquire.bits
+      out.acquire.bits.addr_block := Cat(in_addr_block, UInt(i, idBits))
+      in.grant <> out.grant
+    }
+  }
+}
+
+object ChannelAddressMapper {
+  def map(in: Seq[ClientUncachedTileLinkIO])(implicit p: Parameters): Seq[ClientUncachedTileLinkIO] = {
+    val mapper = Module(new ChannelAddressMapper(in.size))
+    mapper.io.in <> in
+    mapper.io.out
+  }
+
+  def unmap(in: Seq[ClientUncachedTileLinkIO])(implicit p: Parameters): Seq[ClientUncachedTileLinkIO] = {
+    val unmapper = Module(new ChannelAddressUnmapper(in.size))
+    unmapper.io.in <> in
+    unmapper.io.out
+  }
+}
