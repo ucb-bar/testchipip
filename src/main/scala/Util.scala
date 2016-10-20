60edf5a80cf26f5800a74f87abdab81ce15b3270
Ben Keller <bkeller@eecs.berkeley.edu>
2016-10-20 13:51:50 -0700

    Add WideCounterModule

--- src/main/scala/Util.scala
+++ src/main/scala/Util.scala
@@ -1,7 +1,8 @@
 package testchipip
 
+import chisel3.util.ShiftRegister
+import util.WideCounter
 import chisel3._
-import chisel3.util._
 
 class ResetSync(c: Clock, lat: Int = 2) extends Module(_clock = c) {
   val io = new Bundle {
@@ -12,10 +13,26 @@ class ResetSync(c: Clock, lat: Int = 2) extends Module(_clock = c) {
 }
 
 object ResetSync {
-  def apply(r: Bool, c: Clock): Bool =  {
+  def apply(r: Bool, c: Clock): Bool = {
     val sync = Module(new ResetSync(c,2))
     sync.suggestName("resetSyncInst")
     sync.io.reset := r
     sync.io.reset_sync
+  }
+}
+
+// As WideCounter, but it's a module so it can take arbitrary clocks
+class WideCounterModule(w: Int, inc: UInt = UInt(1), reset: Boolean = true, clockSignal: Clock = null, resetSignal: Bool = null)
+    extends Module(Option(clockSignal), Option(resetSignal)) {
+  val io = new Bundle {
+    val value = UInt(OUTPUT, width = w)
+  }
+  io.value := WideCounter(w, inc, reset)
+}
+
+object WideCounterModule {
+  def apply(w: Int, c: Clock, r: Bool) = {
+    val counter = Module(new WideCounterModule(w, clockSignal = c, resetSignal = r))
+    counter.io.value
   }
 }
