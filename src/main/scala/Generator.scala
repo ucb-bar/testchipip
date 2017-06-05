8bd439f37b0e80a045ab35558d044ca5020e7edd
Howard Mao <zhehao.mao@gmail.com>
2017-06-05 15:06:58 -0700

    add GeneratorApp for chisel3 generation

--- src/main/scala/Generator.scala
+++ src/main/scala/Generator.scala
@@ -1,6 +1,35 @@
 package testchipip
+import _root_.util.{HasGeneratorUtilities, ParsedInputNames}
+import config.Parameters
+import chisel3.{Module, Driver}
+import java.io.File
 
-object Generator extends util.GeneratorApp {
-  val longName = names.topModuleClass + "." + names.configs
+trait GeneratorApp extends App with HasGeneratorUtilities {
+  lazy val names = ParsedInputNames(
+    targetDir = args(0),
+    topModuleProject = args(1),
+    topModuleClass = args(2),
+    configProject = args(3),
+    configs = args(4))
+
+  lazy val config = getConfig(names)
+  lazy val world = config.toInstance
+  lazy val params = Parameters.root(world)
+  lazy val circuit = Driver.elaborate(() =>
+      Class.forName(names.fullTopModuleClass)
+        .getConstructor(classOf[Parameters])
+        .newInstance(params)
+        .asInstanceOf[Module])
+
+  lazy val longName = names.topModuleProject + "." +
+                 names.topModuleClass + "." +
+                 names.configs
+
+  def generateFirrtl =
+    Driver.dumpFirrtl(circuit,
+      Some(new File(names.targetDir, s"$longName.fir")))
+}
+
+object Generator extends GeneratorApp {
   generateFirrtl
 }
