d49de5d2bef259e94a245e164ed35bae60d8e984
John Wright <johnwright@eecs.berkeley.edu>
2019-03-06 12:00:00 -0800

    Use Seq of params

--- src/main/scala/tsiHost/TsiHostPeriphery.scala
+++ src/main/scala/tsiHost/TsiHostPeriphery.scala
@@ -13,13 +13,15 @@ import freechips.rocketchip.amba.axi4._
 /**
  * Configuration parameter to configure the TSI Host Widget
  */
-case object PeripheryTSIHostKey extends Field[TSIHostParams]
+case object PeripheryTSIHostKey extends Field[Seq[TSIHostParams]]
 
 /**
  * Trait to create a TSI Host Widget
  */
 trait HasPeripheryTSIHostWidget { this: BaseSubsystem =>
-  val tsiHostWidget = TLTSIHostWidget.attach(TSIHostWidgetAttachParams(p(PeripheryTSIHostKey), sbus, memBuses))
+  val tsiHostWidgetNodes = p(PeripheryTSIHostKey).map { ps =>
+    TLTSIHostWidget.attach(TSIHostWidgetAttachParams(ps, sbus, memBuses))
+  }
 }
 
 /**
@@ -31,8 +33,5 @@ trait HasPeripheryTSIHostWidgetImp extends LazyModuleImp {
   implicit val p: Parameters
 
   // i/o out to the outside world
-  val tsiHostIO = IO(new TSIHostWidgetIO(p(PeripheryTSIHostKey).serialIfWidth))
-
-  // connect to inner modules to the outside (punch through to top)
-  tsiHostIO.serial <> outer.tsiHostWidget.module.io.serial
+  val tsi = outer.tsiHostWidgetNodes.map { case n => IO(new TSIHostWidgetIO(n.params.serialIfWidth)) }
 }
