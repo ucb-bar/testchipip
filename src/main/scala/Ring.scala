694ec716723e62eeba4bf5a74667364398370066
Howard Mao <zhehao.mao@gmail.com>
2019-09-13 14:36:42 -0700

    separate topology-independent code from TLRingNetwork

--- src/main/scala/Ring.scala
+++ src/main/scala/Ring.scala
@@ -3,26 +3,15 @@ package testchipip
 import chisel3._
 import chisel3.util._
 
-import freechips.rocketchip.config._
-import freechips.rocketchip.devices.tilelink.DevNullParams
+import freechips.rocketchip.config.Parameters
 import freechips.rocketchip.diplomacy._
-import freechips.rocketchip.tilelink._
 import freechips.rocketchip.subsystem.{SystemBus, SystemBusParams}
+import freechips.rocketchip.tilelink._
 import freechips.rocketchip.util.{HellaPeekingArbiter, BooleanToAugmentedBoolean}
-
-class TLRingBundle[T <: TLChannel](
-    nNodes: Int, payloadTyp: T) extends Bundle {
-  val ringId = UInt(log2Ceil(nNodes).W)
-  val payload = payloadTyp.cloneType
-  val last = Bool()
-
-  override def cloneType =
-    new TLRingBundle(nNodes, payloadTyp).asInstanceOf[this.type]
-}
 
 class TLRingInputNode[T <: TLChannel](
     nNodes: Int, payloadTyp: T, buffer: BufferParams) extends Module {
-  val bundleType = new TLRingBundle(nNodes, payloadTyp)
+  val bundleType = new TLNetworkBundle(nNodes, payloadTyp)
   val io = IO(new Bundle {
     val ext_in = Flipped(Decoupled(bundleType))
     val int_in = Flipped(Decoupled(bundleType))
@@ -30,14 +19,14 @@ class TLRingInputNode[T <: TLChannel](
   })
 
   val extArb = Module(new HellaPeekingArbiter(
-    bundleType, 2, (b: TLRingBundle[T]) => b.last))
+    bundleType, 2, (b: TLNetworkBundle[T]) => b.last))
   extArb.io.in <> Seq(io.ext_in, io.int_in)
   io.int_out <> buffer(extArb.io.out)
 }
 
 class TLRingOutputNode[T <: TLChannel](
     id: Int, nNodes: Int, payloadTyp: T, buffer: BufferParams) extends Module {
-  val bundleType = new TLRingBundle(nNodes, payloadTyp)
+  val bundleType = new TLNetworkBundle(nNodes, payloadTyp)
   val io = IO(new Bundle {
     val ext_out = Decoupled(bundleType)
     val int_in = Flipped(Decoupled(bundleType))
@@ -46,7 +35,7 @@ class TLRingOutputNode[T <: TLChannel](
 
   val intIn = buffer(io.int_in)
 
-  val intInMatch = intIn.bits.ringId === id.U
+  val intInMatch = intIn.bits.netId === id.U
   io.ext_out.valid := intIn.valid && intInMatch
   io.ext_out.bits := intIn.bits
   io.int_out.valid := intIn.valid && !intInMatch
@@ -58,7 +47,7 @@ class TLRing[T <: TLChannel](
     nIn: Int, nOut: Int, payloadTyp: T,
     buffer: BufferParams, inputFirst: Boolean) extends Module {
   val nNodes = nIn + nOut
-  val bundleType = new TLRingBundle(nNodes, payloadTyp)
+  val bundleType = new TLNetworkBundle(nNodes, payloadTyp)
 
   val io = IO(new Bundle {
     val in = Flipped(Vec(nIn, Decoupled(bundleType)))
@@ -116,7 +105,7 @@ class TLRingNetwork(buffer: BufferParams = BufferParams.default)
         })
     })
 
-  lazy val module = new LazyModuleImp(this) {
+  lazy val module = new LazyModuleImp(this) with HasTLNetwork {
     val (io_in, edgesIn) = node.in.unzip
     val (io_out, edgesOut) = node.out.unzip
     val nIn = edgesIn.size
@@ -124,8 +113,9 @@ class TLRingNetwork(buffer: BufferParams = BufferParams.default)
     val nNodes = nIn + nOut
     val idBits = log2Ceil(nNodes)
 
-    val commonBundle = TLBundleParameters.union(
-      edgesIn.map(_.bundle) ++ edgesOut.map(_.bundle))
+    val forwardIds = (0 until nOut).map(id => (id + nIn).U(idBits.W))
+    val backwardIds = (0 until nIn).map(_.U(idBits.W))
+    val networkName = "TLRingNetwork"
 
     val aRing = Module(new TLRing(
       nIn, nOut, new TLBundleA(commonBundle),
@@ -147,187 +137,22 @@ class TLRingNetwork(buffer: BufferParams = BufferParams.default)
       nIn, nOut, new TLBundleE(commonBundle),
       buffer, true))
 
-    def filter[T](data: Seq[T], mask: Seq[Boolean]) =
-      (data zip mask).filter(_._2).map(_._1)
-
-    val inputIdRanges = TLXbar.mapInputIds(edgesIn.map(_.client))
-    val outputIdRanges = TLXbar.mapOutputIds(edgesOut.map(_.manager))
-    val reachabilityMatrix = edgesIn.map(
-      edgeIn => edgesOut.map(
-        edgeOut => edgeIn.client.clients.exists(
-          client => edgeOut.manager.managers.exists(
-            man => client.visibility.exists(
-              caddr => man.address.exists(
-                maddr => caddr.overlaps(maddr)))))))
-    val canRelease = edgesIn.map(_.client.anySupportProbe)
-    val canProbe = edgesOut.map(_.manager.anySupportAcquireB)
-
-    def wrap[T <: TLChannel](
-        ring: DecoupledIO[TLRingBundle[T]], tl: DecoupledIO[T],
-        selects: Seq[Bool], ids: Seq[UInt], edge: TLEdge,
-        sourceStart: BigInt = -1, sinkStart: BigInt = -1,
-        connect: Boolean = true) {
-      if (connect) {
-        ring.valid := tl.valid
-        ring.bits.ringId := Mux1H(selects, ids)
-        ring.bits.payload := tl.bits
-        ring.bits.last := edge.last(tl)
-
-        if (sourceStart != -1 || sinkStart != -1) {
-          (ring.bits.payload, tl.bits) match {
-            case (ringA: TLBundleA, tlA: TLBundleA) =>
-              ringA.source := tlA.source | sourceStart.U
-            case (ringC: TLBundleC, tlC: TLBundleC) =>
-              ringC.source := tlC.source | sourceStart.U
-            case (ringD: TLBundleD, tlD: TLBundleD) =>
-              ringD.sink := tlD.sink | sinkStart.U
-          }
-        }
-
-        tl.ready := ring.ready
-      } else {
-        ring.valid := false.B
-        ring.bits := DontCare
-        tl.ready := false.B
-      }
+    io_in.zipWithIndex.foreach { case (in, i) =>
+      connectInput(i, in,
+        aRing.io.in(i),
+        bRing.io.out(i),
+        cRing.io.in(i),
+        dRing.io.out(i),
+        eRing.io.in(i))
     }
 
-    def trim(id: UInt, size: Int) =
-      if (size <= 1) 0.U else id(log2Ceil(size)-1, 0)
-
-    def unwrap[T <: TLChannel](
-        tl: DecoupledIO[T], ring: DecoupledIO[TLRingBundle[T]],
-        idSize: Int = 0,
-        connect: Boolean = true) {
-      if (connect) {
-        tl.valid := ring.valid
-        tl.bits := ring.bits.payload
-        if (idSize > 0) {
-          (tl.bits, ring.bits.payload) match {
-            case (tlB: TLBundleB, ringB: TLBundleB) =>
-              tlB.source := trim(ringB.source, idSize)
-            case (tlD: TLBundleD, ringD: TLBundleD) =>
-              tlD.source := trim(ringD.source, idSize)
-            case (tlE: TLBundleE, ringE: TLBundleE) =>
-              tlE.sink := trim(ringE.sink, idSize)
-          }
-        }
-        ring.ready := tl.ready
-      } else {
-        tl.valid := false.B
-        tl.bits := DontCare
-        ring.ready := false.B
-      }
-    }
-
-    (io_in zip edgesIn).zipWithIndex.foreach {
-      case ((in, edgeIn), i) =>
-        val inRange = inputIdRanges(i)
-        val reachable = reachabilityMatrix(i)
-        val probing = reachable.zip(canProbe).map { case (r, p) => r && p}
-
-        val outIds = (0 until nOut).map(id => (id + nIn).U(idBits.W))
-        val portAddrs = edgesOut.map(_.manager.managers.flatMap(_.address))
-        val routingMask = AddressDecoder(filter(portAddrs, reachable))
-        val routeAddrs = portAddrs.map(seq =>
-            AddressSet.unify(seq.map(_.widen(~routingMask)).distinct))
-        val routeFuncs = routeAddrs.map(seq =>
-            (addr: UInt) => seq.map(_.contains(addr)).reduce(_ || _))
-
-        val aMatches = filter(routeFuncs, reachable).map(
-          route => route(in.a.bits.address))
-        val cMatches = filter(routeFuncs, probing).map(
-          route => route(in.c.bits.address))
-        val eMatches = filter(outputIdRanges, probing).map(
-          range => range.contains(in.e.bits.sink))
-
-        val acquireIds = filter(outIds, reachable)
-        val releaseIds = filter(outIds, probing)
-
-        assert(!in.a.valid || PopCount(aMatches) === 1.U,
-          s"TLRingNetwork: Multiple or no matching routes for A channel $i")
-        assert(!in.c.valid || PopCount(cMatches) === 1.U,
-          s"TLRingNetwork: Multiple or no matching routes for C channel $i")
-        assert(!in.e.valid || PopCount(eMatches) === 1.U,
-          s"TLRingNetwork: Multiple or no matching routes for E channel $i")
-
-        val connectBCE = canRelease(i)
-
-        wrap(
-          ring = aRing.io.in(i),
-          tl = in.a,
-          selects = aMatches,
-          ids = acquireIds,
-          edge = edgeIn,
-          sourceStart = inRange.start)
-
-        unwrap(in.b, bRing.io.out(i), inRange.size, connectBCE)
-
-        wrap(
-          ring = cRing.io.in(i),
-          tl = in.c,
-          selects = cMatches,
-          ids = releaseIds,
-          edge = edgeIn,
-          sourceStart = inRange.start,
-          connect = connectBCE)
-
-        unwrap(in.d, dRing.io.out(i), inRange.size)
-
-        wrap(
-          ring = eRing.io.in(i),
-          tl = in.e,
-          selects = eMatches,
-          ids = releaseIds,
-          edge = edgeIn,
-          connect = connectBCE)
-    }
-
-    (io_out zip edgesOut).zipWithIndex.foreach {
-      case ((out, edgeOut), i) =>
-        val outRange = outputIdRanges(i)
-        val reachable = reachabilityMatrix.map(seq => seq(i))
-        val probeable = reachable.zip(canRelease).map { case (r, p) => r && p }
-        val inIds = (0 until nIn).map(id => id.U(idBits.W))
-        val routeFuncs = inputIdRanges.map(range =>
-            (source: UInt) => range.contains(source))
-
-        val bMatches = filter(routeFuncs, probeable).map(
-          route => route(out.b.bits.source))
-        val dMatches = filter(routeFuncs, reachable).map(
-          route => route(out.d.bits.source))
-
-        assert(!out.b.valid || PopCount(bMatches) === 1.U,
-          s"TLRingNetwork: Multiple or no matching routes for B channel $i")
-        assert(!out.d.valid || PopCount(dMatches) === 1.U,
-          s"TLRingNetwork: Multiple or no matching routes for D channel $i")
-
-        val grantIds = filter(inIds, reachable)
-        val probeIds = filter(inIds, probeable)
-
-        val connectBCE = canProbe(i)
-
-        unwrap(out.a, aRing.io.out(i))
-
-        wrap(
-          ring = bRing.io.in(i),
-          tl = out.b,
-          selects = bMatches,
-          ids = probeIds,
-          edge = edgeOut,
-          connect = connectBCE)
-
-        unwrap(out.c, cRing.io.out(i), connect = connectBCE)
-
-        wrap(
-          ring = dRing.io.in(i),
-          tl = out.d,
-          selects = dMatches,
-          ids = grantIds,
-          edge = edgeOut,
-          sinkStart = outRange.start)
-
-        unwrap(out.e, eRing.io.out(i), outRange.size, connectBCE)
+    io_out.zipWithIndex.foreach { case (out, i) =>
+      connectOutput(i, out,
+        aRing.io.out(i),
+        bRing.io.in(i),
+        cRing.io.out(i),
+        dRing.io.in(i),
+        eRing.io.out(i))
     }
   }
 }
