3a758d4d301decf7218c22ff85c6433b8d0fd3be
Howard Mao <zhehao.mao@gmail.com>
2020-04-22 14:17:37 -0700

    delete Generator object

--- src/main/scala/Generator.scala
+++
@@ -1,7 +0,0 @@
-package testchipip
-
-import freechips.rocketchip.config.Parameters
-import firrtl.options.{StageMain}
-import freechips.rocketchip.system.{RocketChipStage}
-
-object Generator extends StageMain(new RocketChipStage)
