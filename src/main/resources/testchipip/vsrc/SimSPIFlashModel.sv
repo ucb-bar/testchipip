888a4547adbf6387da7fb79c85f2f75cdcca6534
John Wright <johnwright@eecs.berkeley.edu>
2020-05-07 02:49:49 -0700

    Split SPI flash model into multiple submodules and add write support

--- src/main/resources/testchipip/vsrc/SimSPIFlashModel.sv
+++ src/main/resources/testchipip/vsrc/SimSPIFlashModel.sv
@@ -1,18 +1,7 @@
-
-import "DPI-C" function void spi_flash_read(
-    input longint ptr,
-    input int address,
-    output byte data
-);
-
-import "DPI-C" function longint spi_flash_init(
-    input string filename,
-    input int max_addr
-);
-
 module SimSPIFlashModel #(
-    parameter MAX_ADDR,
-    parameter ID
+    parameter string PLUSARG,
+    parameter bit READONLY,
+    parameter longint CAPACITY_BYTES
 ) (
     input sck,
     input cs_0,
@@ -23,178 +12,50 @@ module SimSPIFlashModel #(
     inout dq_3
 );
 
-    // CAUTION! This model only supports a small subset of standard QSPI flash
-    // features. It is useful for modeling a pre-loaded flash memory intended
-    // to be used as a ROM. You should replace this with a verilog model of
-    // your specific flash device for more rigorous verification. It also very
-    // likely contains bugs. Use at your own risk!
+    wire [3:0] dq_in;
+    wire [3:0] dq_out;
+    wire [3:0] dq_drive;
 
-    // Supported SPI instructions
-    // 3-byte address reads
-    localparam CMD_READ               = 8'h03; // No dummy
-    localparam CMD_FAST_READ          = 8'h0B;
-    localparam CMD_QUAD_O_FAST_READ   = 8'h6B;
-    localparam CMD_QUAD_IO_FAST_READ  = 8'hEB;
-    // 4-byte address reads
-    localparam CMD_READ4              = 8'h13; // No dummy
-    localparam CMD_FAST_READ4         = 8'h0C;
-    localparam CMD_QUAD_O_FAST_READ4  = 8'h6C;
-    localparam CMD_QUAD_IO_FAST_READ4 = 8'hEC;
-    // No writes/erases are supported by this model yet
-    // No register reads are supported by this model yet
+    wire mem_req_valid, mem_req_r_wb;
+    wire [7:0] mem_req_data;
+    wire [7:0] mem_resp_data;
+    wire [$clog2(CAPACITY_BYTES)-1:0] mem_req_addr;
 
-    // SPI mode settings for clock polarity and phase
-    localparam CPOL = 1'b0;
-    localparam CPHA = 1'b0;
-    // CS active low by default, but set this to 1 to invert
-    localparam CSXOR = 1'b0;
+    SPIFlashMemCtrl #(
+        .ADDR_BITS($clog2(CAPACITY_BYTES))
+    ) ctrl (
+        .sck(sck),
+        .cs(cs_0),
+        .reset(reset),
+        .dq_in(dq_in),
+        .dq_drive(dq_drive),
+        .dq_out(dq_out),
+        .mem_req_valid(mem_req_valid),
+        .mem_req_addr(mem_req_addr),
+        .mem_req_data(mem_req_data),
+        .mem_req_r_wb(mem_req_r_wb),
+        .mem_resp_data(mem_resp_data)
+    );
 
-    // SPI flash behavior settings
-    // Often the number of dummy cycles depends on the instruction, but here
-    // we'll make it all uniform.
-    localparam DUMMY_CYCLES = 8;
+    plusarg_file_mem #(
+        .PLUSARG(PLUSARG),
+        .ADDR_BITS($clog2(CAPACITY_BYTES)),
+        .READONLY(READONLY),
+        .DATA_BYTES(1)
+    ) memory (
+        .clock(sck),
+        .reset(reset),
+        .mem_req_valid(mem_req_valid),
+        .mem_req_addr(mem_req_addr),
+        .mem_req_data(mem_req_data),
+        .mem_req_r_wb(mem_req_r_wb),
+        .mem_resp_data(mem_resp_data)
+    );
 
-    // Stores the file name for the binary memory contents of the flash
-    string filename;
-
-    // This is a pointer to the C SPIFlashMem object
-    longint dev_ptr;
-
-    initial begin
-        if (MAX_ADDR > 33'h0_ffff_ffff) begin /* verilator lint_off CMPCONST */
-            // Workaround for verilator to write to STDERR
-            $fwrite(32'h80000002, "SimSPIFlashModel supports only 32-bit memory addressing.\n");
-            $fatal;
-        end
-        // +spiflash0=/path/to/file for SPI flash 0, etc.
-        if ($value$plusargs($sformatf("spiflash%0d=%%s", ID), filename)) begin
-            dev_ptr = spi_flash_init(filename, MAX_ADDR);
-        end else begin
-            // Workaround for verilator to write to STDERR
-            $fwrite(32'h80000002, "No memory image provided for SPI flash %0d. Use +spiflash%0d=<file> to specify.\n", ID, ID);
-            $fatal;
-        end
-    end
-
-    // State
-    reg [2:0] state;
-    logic [2:0] state_next;
-    // States
-    localparam STANDBY  = 3'd0;
-    localparam GET_CMD  = 3'd1;
-    localparam GET_ADDR = 3'd2;
-    localparam DUMMY    = 3'd3;
-    localparam PUT_DATA = 3'd4;
-
-    // Incoming data
-    reg [31:0] data_buf;
-    logic [31:0] data_buf_next;
-    // Incoming data bit count
-    reg [5:0] data_count;
-    logic [5:0] data_count_next;
-    // Dummy cycle counter
-    reg [7:0] dummy;
-    logic [7:0] dummy_next;
-    // Command
-    reg [7:0] cmd;
-    logic [7:0] cmd_next;
-    // Address
-    reg [31:0] addr;
-    logic [31:0] addr_next;
-
-    // Internal clock to deal with clock polarity
-    logic clock, cs;
-    assign clock = CPOL ^ sck;
-    assign cs = CSXOR ^ cs_0;
-
-    // Quad data stuff
-    logic quad_io, quad_i, quad_o;
-
-    assign quad_i = (cmd_next == CMD_QUAD_IO_FAST_READ) || (cmd_next == CMD_QUAD_IO_FAST_READ4);
-    assign quad_o = quad_i || (cmd_next == CMD_QUAD_O_FAST_READ) || (cmd_next == CMD_QUAD_O_FAST_READ4);
-    assign quad_io = (quad_i && (state_next == GET_ADDR)) || (quad_o && (state_next == PUT_DATA));
-
-    // State machine stuff
-    logic addr_4byte, cmd_done, addr_done, dummy_done, cmd_has_dummy, cmd_valid, incr_addr;
-
-    assign addr_4byte = (cmd == CMD_READ4) || (cmd == CMD_FAST_READ4) || (cmd == CMD_QUAD_O_FAST_READ4) || (cmd == CMD_QUAD_IO_FAST_READ4);
-    assign cmd_valid =
-        (cmd_next == CMD_READ)  || (cmd_next == CMD_FAST_READ)  || (cmd_next == CMD_QUAD_O_FAST_READ) || (cmd_next == CMD_QUAD_IO_FAST_READ) ||
-        (cmd_next == CMD_READ4) || (cmd_next == CMD_FAST_READ4) || (cmd_next == CMD_QUAD_O_FAST_READ4) || (cmd_next == CMD_QUAD_IO_FAST_READ4);
-    assign cmd_done  = (state == GET_CMD) && (data_count == 6'd8);
-    assign addr_done = (state == GET_ADDR) && (data_count == (addr_4byte ? 6'd40 : 6'd32));
-    assign dummy_done = dummy == (DUMMY_CYCLES - 1);
-    assign cmd_has_dummy = (cmd != CMD_READ) && (cmd != CMD_READ4);
-    assign incr_addr = (state == PUT_DATA) && (data_count[2:0] == 3'b000); // We let the counter free run...this just increments every byte
-
-    assign data_buf_next = quad_io ? {data_buf[27:0], dq_3, dq_2, dq_1, dq_0} : {data_buf[30:0], dq_0};
-    assign data_count_next = (state_next == DUMMY) ? data_count : data_count + (quad_io ? 6'd4 : 6'd1) ;
-    assign dummy_next = (state == DUMMY) ? dummy + 8'd1 : 8'd0 ;
-
-    always_comb begin
-        if (state == DUMMY) addr_next = addr;
-        else if (addr_done) addr_next = addr_4byte ? data_buf : (data_buf & 32'h00ff_ffff);
-        else if (incr_addr) addr_next = addr + 32'd1;
-        else addr_next = addr;
-    end
-
-    assign cmd_next = cmd_done ? data_buf[7:0] : cmd ;
-
-    // Output driving
-    reg [7:0] data_out;
-    reg drive_dq;
-    logic [3:0] dq_out;
-
-    assign dq_out[3] = data_out[7];
-    assign dq_out[2] = data_out[6];
-    assign dq_out[1] = quad_io ? data_out[5] : data_out[7] ;
-    assign dq_out[0] = data_out[4];
-
-    assign dq_3 = (drive_dq && quad_io) ? dq_out[3] : 1'bz;
-    assign dq_2 = (drive_dq && quad_io) ? dq_out[2] : 1'bz;
-    assign dq_1 =  drive_dq             ? dq_out[1] : 1'bz;
-    assign dq_0 = (drive_dq && quad_io) ? dq_out[0] : 1'bz;
-
-    // Not using always_ff to get around a verilator issue
-    always @(posedge clock or negedge clock or posedge reset or posedge cs) begin
-        if (reset | cs) begin
-            data_count <= 6'd0;
-            state <= STANDBY;
-            dummy <= 8'd0;
-            drive_dq <= 1'b0;
-            cmd <= 8'd0;
-        end else begin
-            if (clock ^ CPHA) begin
-                // Capture edge
-                data_buf    <= data_buf_next;
-                data_count  <= data_count_next;
-                state       <= state_next;
-                dummy       <= dummy_next;
-                addr        <= addr_next;
-                cmd         <= cmd_next;
-            end else begin
-                // Launch edge
-                drive_dq <= (state_next == PUT_DATA);
-                if ((data_count[2:0] == 3'b000) && (state_next == PUT_DATA)) begin
-                    spi_flash_read(dev_ptr, addr_next, data_out);
-                end else begin
-                    // Note: Not using <= because the DPI call is blocking,
-                    // and verilator requires them to be the same
-                    data_out = quad_io ? {data_out[3:0], 4'd0} : {data_out[6:0], 1'd0} ;
-                end
-            end
-        end
-    end
-
-    always_comb begin
-        casez(state)
-            STANDBY:  state_next = cs         ? STANDBY  : GET_CMD ;
-            GET_CMD:  state_next = cmd_done   ? (cmd_valid ? GET_ADDR : STANDBY) : GET_CMD ;
-            GET_ADDR: state_next = addr_done  ? (cmd_has_dummy ? DUMMY : PUT_DATA) : GET_ADDR ;
-            DUMMY:    state_next = dummy_done ? PUT_DATA : DUMMY ;
-            PUT_DATA: state_next = cs         ? STANDBY  : PUT_DATA ;
-            default:  state_next = STANDBY;
-        endcase
-    end
+    assign dq_3 = (dq_drive[3] ? dq_out[3] : 1'bz);
+    assign dq_2 = (dq_drive[2] ? dq_out[2] : 1'bz);
+    assign dq_1 = (dq_drive[1] ? dq_out[1] : 1'bz);
+    assign dq_0 = (dq_drive[0] ? dq_out[0] : 1'bz);
+    assign dq_in = {dq_3, dq_2, dq_1, dq_0};
 
 endmodule
--- src/main/scala/SPIFlash.scala
+++ src/main/scala/SPIFlash.scala
@@ -2,7 +2,7 @@ package testchipip
 
 import chisel3._
 import chisel3.util.{HasBlackBoxResource}
-import chisel3.experimental.{Analog, IntParam}
+import chisel3.experimental.{Analog, IntParam, StringParam}
 
 import freechips.rocketchip.config.{Parameters}
 import freechips.rocketchip.util.{PlusArgArtefacts}
@@ -18,28 +18,65 @@ class SPIFlashIO extends SPIChipIO(1) {
   val reset = Output(Reset()) // This is Output because we're going to flip it in the BlackBox
 }
 
-class SimSPIFlashModel(capacityBytes: BigInt, id: Int) extends BlackBox(Map(
-  "MAX_ADDR" -> IntParam(capacityBytes-1),
-  "ID" -> IntParam(id))
+object SPIFlashPlusarg {
+  def apply(i: Int) = s"spiflash${i}"
+}
+
+class SimSPIFlashModel(capacityBytes: BigInt, id: Int, rdOnly: Boolean = true) extends BlackBox(Map(
+  "PLUSARG" -> StringParam(SPIFlashPlusarg(id)),
+  "READONLY" -> IntParam(if (rdOnly) 1 else 0),
+  "CAPACITY_BYTES" -> IntParam(capacityBytes))
 ) with HasBlackBoxResource {
   val io = IO(Flipped(new SPIFlashIO()))
 
   // The model adds a +spiflash<ID>=<path> plusarg. It's not a numeric, so we can't use
   // plusarg_reader, but we can still add to the PlusArgArtefacts
   // Unfortunately it requires a numeric default, but that's really just a docstring issue
-  PlusArgArtefacts.append(s"spiflash${id}", 0, s"Binary image to mount to SPI flash memory ${id}")
+  PlusArgArtefacts.append(SPIFlashPlusarg(id), 0, s"Binary image to mount to SPI flash memory ${id}")
 
   require(capacityBytes < 0x100000000L, "SimSPIFlashModel only supports 32-bit addressing")
 
   addResource("/testchipip/vsrc/SimSPIFlashModel.sv")
-  addResource("/testchipip/csrc/SimSPIFlashModel.cc")
-  addResource("/testchipip/csrc/SimSPIFlashModel.h")
+  addResource("/testchipip/vsrc/SPIFlashMemCtrl.sv")
+  addResource("/testchipip/vsrc/plusarg_file_mem.sv")
+  addResource("/testchipip/csrc/plusarg_file_mem.cc")
+  addResource("/testchipip/csrc/plusarg_file_mem.h")
+}
+
+class SPIFlashMemCtrlIO(val addrBits: Int) extends Bundle {
+  val sck = Input(Bool())
+  val cs = Input(Bool())
+  val reset = Input(Bool())
+  val dq_in = Input(UInt(4.W))
+  val dq_drive = Output(UInt(4.W))
+  val dq_out = Output(UInt(4.W))
+  val mem = new MemCtrlIO(addrBits, 8)
+}
+
+class MemCtrlIO(val addrBits: Int, val dataBits: Int) extends Bundle {
+  val req = new Bundle {
+    val valid = Output(Bool())
+    val r_wb = Output(Bool())
+    val addr = Output(UInt(addrBits.W))
+    val data = Output(UInt(dataBits.W))
+  }
+  val resp = new Bundle {
+    val data = Input(UInt(dataBits.W))
+  }
+}
+
+class SPIFlashMemCtrl(addrBits: Int) extends BlackBox(Map(
+  "ADDR_BITS" -> IntParam(addrBits))
+) with HasBlackBoxResource {
+  val io = IO(new SPIFlashMemCtrlIO(addrBits))
+
+  addResource("/testchipip/vsrc/SPIFlashMemCtrl.sv")
 }
 
 object SimSPIFlashModel {
-  def connect(spi: Seq[SPIChipIO], reset: Reset)(implicit p: Parameters) {
+  def connect(spi: Seq[SPIChipIO], reset: Reset, rdOnly: Boolean = true)(implicit p: Parameters) {
     spi.zip(p(PeripherySPIFlashKey)).zipWithIndex.foreach { case ((port, params), i) =>
-      val spi_mem = Module(new SimSPIFlashModel(params.fSize, i))
+      val spi_mem = Module(new SimSPIFlashModel(params.fSize, i, rdOnly))
       spi_mem.suggestName(s"spi_mem_${i}")
       spi_mem.io.sck := port.sck
       require(params.csWidth == 1, "I don't know what to do with your extra CS bits. Fix me please.")
