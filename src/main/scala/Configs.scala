531ffb7020ef012c5d9dd8642df515d2fbadd7a8
Abraham Gonzalez <abe.gonzalez@berkeley.edu>
2021-03-03 19:44:01 +0000

    Add ability to change BlockDevice location

--- src/main/scala/Configs.scala
+++ src/main/scala/Configs.scala
@@ -44,6 +44,10 @@ class WithBlockDevice extends Config((site, here, up) => {
   case BlockDeviceKey => Some(BlockDeviceConfig())
 })
 
+class WithBlockDeviceLocations(slaveWhere: TLBusWrapperLocation = PBUS, masterWhere: TLBusWrapperLocation = FBUS) extends Config((site, here, up) => {
+  case BlockDeviceAttachKey => BlockDeviceAttachParams(slaveWhere, masterWhere)
+})
+
 class WithNBlockDeviceTrackers(n: Int) extends Config((site, here, up) => {
   case BlockDeviceKey => up(BlockDeviceKey, site) match {
     case Some(a) => Some(a.copy(nTrackers = n))
