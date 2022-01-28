d5e60afa19451d1a2c24d3dfbb5fbe23bbfb4599
Sihao Liu <sihao@cs.ucla.edu>
2022-01-28 08:08:27 -0800

    [feature] support simv option by prevent them enter htif as argv

--- src/main/resources/testchipip/csrc/SimSerial.cc
+++ src/main/resources/testchipip/csrc/SimSerial.cc
@@ -6,6 +6,24 @@
 
 testchip_tsi_t *tsi = NULL;
 
+// Remove VCS simv option from argv if it match pattern -X???=
+void remove_vcs_simv_opt(int & argc, char **& argv){
+    int idx = 0;
+    while(idx < argc){
+        std::string str = std::string(argv[idx]);
+        if(str.length() > 1 && str[0] == '-' && str[1] != '-' && str.find('=') != std::string::npos){
+            // Found -????=???? as VCS simv option
+            for(int i = idx; i < argc - 1; i++){
+                // Remove the current option
+                argv[i] = argv[i + 1];
+            }
+            argc--;
+        }else{
+            idx++;
+        }
+    }
+}
+
 extern "C" int serial_tick(
         unsigned char out_valid,
         unsigned char *out_ready,
@@ -23,6 +41,9 @@ extern "C" int serial_tick(
         s_vpi_vlog_info info;
         if (!vpi_get_vlog_info(&info))
           abort();
+
+	  // Prevent simv option enter htif
+	  remove_vcs_simv_opt(info.argc, info.argv);
 
         // TODO: We should somehow inspect whether or not our backing memory supports loadmem, instead of unconditionally setting it to true
         tsi = new testchip_tsi_t(info.argc, info.argv, true);
