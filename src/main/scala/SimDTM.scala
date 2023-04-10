efe01073af2e9c0935069ae316a1fbe41e97ae60
Jerry Zhao <jerryz123@berkeley.edu>
2023-04-10 15:40:58 -0700

    Add fork of SimDTM -> TestchipSimDTM

--- src/main/scala/SimDTM.scala
+++ src/main/scala/SimDTM.scala
@@ -2,7 +2,7 @@ package testchipip
 
 import chisel3._
 import chisel3.util._
-import freechips.rocketchip.config.{Parameters}
+import org.chipsalliance.cde.config.{Parameters}
 import freechips.rocketchip.devices.debug.{DMIIO, ClockedDMIIO}
 
 
