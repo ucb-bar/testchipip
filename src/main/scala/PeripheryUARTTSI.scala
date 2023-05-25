bac23971391ed130a8dd93868aa00ce3f982f328
Jerry Zhao <jerryz123@berkeley.edu>
2023-05-24 19:15:00 -0700

    Add UARTTSIIO

--- src/main/scala/PeripheryUARTTSI.scala
+++ src/main/scala/PeripheryUARTTSI.scala
@@ -19,11 +19,17 @@ case class UARTTSIClientParams(
 
 case object UARTTSIClientKey extends Field[Option[UARTTSIClientParams]](None)
 
+class UARTTSIIO(val uartParams: UARTParams) extends Bundle {
+  val uart = new UARTPortIO(uartParams)
+  val dropped = Output(Bool())
+  val tsi2tl_state = Output(UInt())
+}
+
 // This trait adds a UART port to the subsystem that transports TSI.
 // It is supposed to be used for FPGA-harnesses or FPGA prototypes
 // This should not be used for ASIC implemnetations
 trait CanHavePeripheryUARTTSI { this: BaseSubsystem =>
-  val (uart_tsi, tsi2tl_state) = p(UARTTSIClientKey).map { params =>
+  val uart_tsi = p(UARTTSIClientKey).map { params =>
     val tlbus = locateTLBusWrapper(params.tlbus)
     val uartParams = params.uartParams
     val tsi2tl = tlbus { LazyModule(new TSIToTileLink) }
@@ -33,28 +39,18 @@ trait CanHavePeripheryUARTTSI { this: BaseSubsystem =>
       val width_adapter = Module(new SerialWidthAdapter(8, TSI.WIDTH))
       tsi2tl.module.io.tsi.flipConnect(width_adapter.io.wide)
       width_adapter.io.narrow.flipConnect(uart_to_serial.io.serial)
-      val uart_tsi_io = IO(new UARTPortIO(uartParams))
-      uart_tsi_io <> uart_to_serial.io.uart
+      val uart_tsi_io = IO(new UARTTSIIO(uartParams))
+      uart_tsi_io.uart <> uart_to_serial.io.uart
+      uart_tsi_io.dropped := uart_to_serial.io.dropped
+      uart_tsi_io.tsi2tl_state := tsi2tl.module.io.state
       uart_tsi_io
-    } }
-    val tsi2tl_state_bus_io = tlbus { InModuleBody {
-      val tsi2tl_state = IO(Output(UInt()))
-      tsi2tl_state := tsi2tl.module.io.state
-      tsi2tl_state
     } }
 
     val uart_tsi_io = InModuleBody {
-      val uart_tsi_io = IO(new UARTPortIO(uartParams))
+      val uart_tsi_io = IO(new UARTTSIIO(uartParams))
       uart_tsi_io <> uart_bus_io
       uart_tsi_io
     }
-
-    val tsi2tl_state_io = InModuleBody {
-      val tsi2tl_state_io = IO(Output(UInt()))
-      tsi2tl_state_io := tsi2tl_state_bus_io
-      tsi2tl_state_io
-    }
-
-    (uart_tsi_io, tsi2tl_state_io)
-  }.unzip
+    uart_tsi_io
+  }
 }
