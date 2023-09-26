307fca3c3ec4484d071744df6752bbbb77be7ddb
-T.K.- <t_k_233@outlook.com>
2023-09-26 10:50:26 -0700

    FIX: change PC reset vector in cosim

--- src/main/resources/testchipip/csrc/cospike_impl.cc
+++ src/main/resources/testchipip/csrc/cospike_impl.cc
@@ -37,6 +37,9 @@ extern std::map<long long int, backing_data_t> backing_mem_data;
 #define PLIC_BASE (0xc000000)
 #define PLIC_SIZE (0x4000000)
 
+// address of the PC register after reset
+#define RESET_VECTOR (0x10000)
+
 #define COSPIKE_PRINTF(...) {                   \
   printf(__VA_ARGS__);                          \
   fprintf(stderr, __VA_ARGS__);                 \
@@ -251,7 +254,7 @@ int cospike_cosim(long long int cycle,
     sim->configure_log(true, true);
     for (int i = 0; i < info->nharts; i++) {
       // Use our own reset vector
-      sim->get_core(hartid)->get_state()->pc = 0x10040;
+      sim->get_core(hartid)->get_state()->pc = RESET_VECTOR;
       // Set MMU to support up to sv39, as our normal hw configs do
       sim->get_core(hartid)->set_impl(IMPL_MMU_SV48, false);
       sim->get_core(hartid)->set_impl(IMPL_MMU_SV57, false);
