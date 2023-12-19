c020473d56d24b3c182ccfe29d984f42ed92b3f8
Jerry Zhao <jerryz123@berkeley.edu>
2023-12-19 10:03:55 -0800

    Split Configs into files per-subproject

---
+++ src/main/scala/test/Configs.scala
@@ -0,0 +1,19 @@
+package testchipip
+
+import chisel3._
+import org.chipsalliance.cde.config.{Parameters, Config}
+
+class WithTestChipUnitTests extends Config((site, here, up) => {
+  case UnitTests => (testParams: Parameters) =>
+    TestChipUnitTests(testParams)
+})
+
+class WithClockUtilTests extends Config((site, here, up) => {
+  case UnitTests => (testParams: Parameters) => ClockUtilTests()
+})
+
+class TestChipUnitTestConfig extends Config(
+  new WithTestChipUnitTests ++ new BaseConfig)
+
+class ClockUtilTestConfig extends Config(
+  new WithClockUtilTests ++ new BaseConfig)
