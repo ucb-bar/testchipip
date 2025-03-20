0b8d15799d24452fb37996c74a827603d1810781
Lux <iansseijelly@berkeley.edu>
2025-03-19 21:04:05 -0700

    ADD: WIP for TL->CTC

--- src/main/scala/ctc/TileLinkToCTC.scala
+++ src/main/scala/ctc/TileLinkToCTC.scala
@@ -31,4 +31,68 @@ class TileLinkToCTCModule(outer: TileLinkToCTC) extends LazyModuleImp(outer) {
   })
 
   val (mem, edge) = outer.node.in(0)
+
+  // constants
+  val cmdLen = 2
+  val lenLen = 16
+  val wordLen = 64
+  val dataBits = mem.params.dataBits
+  val beatBytes = dataBits / 8
+  val nChunksPerBeat = dataBits / CTC.INNER_WIDTH
+  val byteAddrBits = log2Ceil(beatBytes)
+  val nChunksPerWord = wordLen / CTC.INNER_WIDTH
+
+  val len = Reg(UInt(lenLen.W))
+  val cmd = Reg(UInt(cmdLen.W))
+  val addr = Reg(UInt(wordLen.W))
+  val body = Reg(Vec(nChunksPerBeat, UInt(CTC.INNER_WIDTH.W)))
+
+  val (s_idle :: s_cmd :: s_addr :: s_r_body :: s_r_ack :: Nil) = Enum(6)
+  val state = RegInit(s_idle)
+  val idx = Reg(UInt(log2Up(nChunksPerWord).W))
+
+  // state-driven signals
+  io.flit.in.ready := state.isOneOf(s_r_body)
+  io.flit.out.valid:= state.isOneOf(s_cmd, s_addr)
+  io.flit.out.bits := Mux(state === s_cmd, Cat(cmd, len),
+    Mux(state === s_addr, addr(CTC.INNER_WIDTH - 1, 0), body(idx))) // TODO: add the rest of the data here
+
+  mem.a.ready := state === s_idle
+  mem.b.valid := false.B
+  mem.c.ready := false.B
+  mem.d.valid := state === s_r_ack 
+  mem.e.ready := false.B
+
+  when (state === s_idle && mem.a.valid) {
+    len := mem.a.bits.size
+    cmd := Mux(mem.a.bits.opcode === TLMessages.Get, CTCCommand.read_req, CTCCommand.write_req)
+    addr := mem.a.bits.address
+    state := s_cmd
+  }
+
+  when (state === s_cmd && io.flit.out.ready) {
+    state := s_addr
+  }
+
+  when (state === s_addr && io.flit.out.ready) {
+    addr := (addr >> CTC.INNER_WIDTH) 
+    idx := idx + 1.U
+    when (idx === (nChunksPerWord - 1).U) {
+      idx := 0.U
+      state := s_r_body
+    }
+  }
+
+  when (state === s_r_body && io.flit.in.valid) {
+    body(idx) := io.flit.in.bits.flit
+    idx := idx + 1.U
+    when (idx === len - 1.U) {
+      idx := 0.U
+      state := s_r_ack
+    }
+  }
+
+  when (state === s_r_ack && mem.d.ready) {
+    state := s_idle
+  } 
 }
