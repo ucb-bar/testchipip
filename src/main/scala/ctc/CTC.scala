ba234f71b3baa59e92fc97fb936ad23e8b0949d9
Ella Schwarz <53250063+schwarz-em@users.noreply.github.com>
2025-11-25 17:08:43 -0800

    Add CTC (#263)
    * ADD: initial CTC->TL converter
    
    * ADD: skeleton refactor to TL->CTC
    
    * ADD: WIP for TL->CTC
    
    * ADD: write handling for TL->CTC
    
    * ADD: unittest, fully elaborates
    
    * ADD: somewhat working
    
    * FIX: passes unittest
    
    * FIX: multibeat WIP, pending addr fixes
    
    * FIX: now works for multi-beat
    
    * FIX: accomodate for spec changes
    
    * ADD: min transfersize -> 1
    
    * git commit -m "ADD: implement CTC parameters and integration with DigitalTop, including clock domain management and PHY interface" --trailer "Co-authored-by: -T.K.- <t_k_233@outlook.com>"
    
    * ADD: better params passing
    
    * ADD: support 4byte transactions
    
    * ADD: fix content
    
    * [WIP] make PHY optional
    
    * Add address translator + fix size param
    
    * UPDATE: modify address assignment in CTCToTileLink to incorporate flit data for improved transaction handling
    
    * REFINE: update CTC and TileLink modules to use header length instead of word length, enhancing single-word transaction handling
    
    * Support multiple CTC ports + clean up
    
    ---------
    
    Co-authored-by: Lux <iansseijelly@berkeley.edu>
    Co-authored-by: Ella Schwarz <schwarzem@a6.Millennium.Berkeley.EDU>

---
+++ src/main/scala/ctc/CTC.scala
@@ -0,0 +1,145 @@
+package testchipip.ctc
+
+import chisel3._
+import chisel3.util._
+import chisel3.experimental.dataview._
+
+import org.chipsalliance.cde.config.{Parameters, Field, Config}
+import freechips.rocketchip.subsystem._
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.tilelink._
+import freechips.rocketchip.prci._
+import freechips.rocketchip.util.ResetCatchAndSync
+import testchipip.soc.{InwardAddressTranslator}
+
+import testchipip.serdes._
+
+
+object CTC {
+  val INNER_WIDTH = 32
+  val INNER_WIDTH_BYTES = INNER_WIDTH / 8
+  val OUTER_WIDTH = 4
+}
+
+object CTCCommand {
+  val read_req = 0.U
+  val write_req = 1.U
+  val read_ack = 2.U
+  val write_ack = 3.U
+}
+
+case class CTCParams(
+  onchipAddr: BigInt = 0x100000000L,                      // addresses that get routed here from THIS chip
+  offchipAddr: BigInt = 0x0,                              // addresses that this ctc device can access on the OTHER chip
+  size: BigInt = ((1L << 10) - 1),                        // 1024 bytes
+  managerBus: Option[TLBusWrapperLocation] = Some(SBUS),
+  clientBus: Option[TLBusWrapperLocation] = Some(SBUS),
+  phyFreqMHz: Int = 100,
+  noPhy: Boolean = false
+)
+
+// For using CTC in a chiplet firesim config with no PHY
+class CTCBridgeIO extends Bundle {
+  val client_flit = new DecoupledFlitIO(CTC.INNER_WIDTH) // Driven by client/ctc2tl
+  val manager_flit = new DecoupledFlitIO(CTC.INNER_WIDTH) // Driven by manager/tl2ctc
+}
+
+case object CTCKey extends Field[Seq[CTCParams]](Nil)
+
+trait CanHavePeripheryCTC { this: BaseSubsystem =>
+  private val portName = "ctc"
+
+  val (ctc2tls, tl2ctcs, ctc_ios) = p(CTCKey).zipWithIndex.map {
+    case (params, id) => {
+
+      val ctc_name = s"ctc$id"
+
+      val phyParams = CreditedSourceSyncSerialPhyParams(
+        phitWidth = CTC.OUTER_WIDTH,
+        flitWidth = CTC.INNER_WIDTH,
+        freqMHz = params.phyFreqMHz,
+        flitBufferSz = 16
+      )
+
+      lazy val slave_bus = locateTLBusWrapper(params.managerBus.get)
+      lazy val master_bus = locateTLBusWrapper(params.clientBus.get)
+
+      val ctc_domain = LazyModule(new ClockSinkDomain(name=Some(s"CTC$id")))
+      ctc_domain.clockNode := slave_bus.fixedClockNode
+
+      val translator = ctc_domain {
+        LazyModule(InwardAddressTranslator(AddressSet(params.offchipAddr, params.size), Some(params.onchipAddr))(p))
+      }
+
+      require(slave_bus.dtsFrequency.isDefined,
+        s"Slave bus ${slave_bus.busName} must provide a frequency")
+      require(master_bus.dtsFrequency.isDefined,
+        s"Master bus ${master_bus.busName} must provide a frequency")
+      require(slave_bus.dtsFrequency == master_bus.dtsFrequency,
+        s"Mismatching slave freq ${slave_bus.dtsFrequency} != master freq ${master_bus.dtsFrequency}")
+
+
+      // a TL master/client device
+      val ctc2tl = ctc_domain { LazyModule(new CTCToTileLink(portId=id)(p)) }
+      // a TL slave/manager device
+      val tl2ctc = ctc_domain { LazyModule(new TileLinkToCTC(baseAddr=params.offchipAddr, size=params.size)(p)) }
+
+      slave_bus.coupleTo(portName) { translator(tl2ctc.node) := TLBuffer() := _ }
+      master_bus.coupleFrom(portName) { _ := TLBuffer() := ctc2tl.node }
+      
+      // If we provide a clock, generate a clock domain for the outgoing clock
+      val serial_tl_clock_freqMHz = CreditedSourceSyncSerialPhyParams().freqMHz
+      val serial_tl_clock_node = ctc_domain { ClockSinkNode(Seq(ClockSinkParameters(take=Some(ClockParameters(serial_tl_clock_freqMHz))))) }
+      serial_tl_clock_node := ClockGroup()(p, ValName(s"${ctc_name}_clock")) := allClockGroupsNode
+
+      val ctc_outer_io = ctc_domain { InModuleBody {
+        if (params.noPhy) {
+          val flit_io = IO(new CTCBridgeIO).suggestName(ctc_name)
+          flit_io.manager_flit <> tl2ctc.module.io.flit
+          flit_io.client_flit <> ctc2tl.module.io.flit
+          flit_io
+        } else {
+          val phit_io = IO(phyParams.genIO).suggestName(ctc_name)
+
+          // 3 clock domains -
+          // - ctc2tl's "Inner clock": synchronizes signals going to the digital logic
+          // - outgoing clock: synchronizes signals going out
+          // - incoming clock: synchronizes signals coming in
+          val outgoing_clock = serial_tl_clock_node.in.head._1.clock
+          val outgoing_reset = ResetCatchAndSync(outgoing_clock, ctc2tl.module.reset.asBool)
+          val incoming_clock = phit_io.clock_in
+          val incoming_reset = ResetCatchAndSync(incoming_clock, phit_io.reset_in.asBool)
+          phit_io.clock_out := outgoing_clock
+          phit_io.reset_out := outgoing_reset.asAsyncReset
+          val phy = Module(new CreditedSerialPhy(2, phyParams))
+          phy.io.incoming_clock := incoming_clock
+          phy.io.incoming_reset := incoming_reset
+          phy.io.outgoing_clock := outgoing_clock
+          phy.io.outgoing_reset := outgoing_reset
+          phy.io.inner_clock := ctc2tl.module.clock
+          phy.io.inner_reset := ctc2tl.module.reset
+          phy.io.inner_ser(0).in <> ctc2tl.module.io.flit.in
+          phy.io.inner_ser(0).out <> tl2ctc.module.io.flit.out
+          phy.io.inner_ser(1).in <> tl2ctc.module.io.flit.in
+          phy.io.inner_ser(1).out <> ctc2tl.module.io.flit.out
+
+          phy.io.outer_ser <> phit_io.viewAsSupertype(new ValidPhitIO(phyParams.phitWidth))
+          phit_io
+        }
+      }}
+
+      val outer_io = InModuleBody {
+        val outer_io = if (params.noPhy) IO(new CTCBridgeIO).suggestName(ctc_name) else IO(phyParams.genIO).suggestName(ctc_name)
+        outer_io <> ctc_outer_io
+        outer_io
+      }
+
+      (ctc2tl, tl2ctc, outer_io)
+    }
+  }.unzip3
+}
+
+
+class WithCTC(params: Seq[CTCParams] = Seq(CTCParams())) extends Config((site, here, up) => {
+  case CTCKey => params
+})
---
+++ src/main/scala/ctc/TileLinkToCTC.scala
@@ -0,0 +1,186 @@
+package testchipip.ctc
+
+import chisel3._
+import chisel3.util._
+import testchipip.serdes._
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.tilelink._
+import freechips.rocketchip.util._
+import org.chipsalliance.cde.config.{Parameters, Field}
+
+// a tl-manager (slave) device
+// from inner: receives read and write requests in TL
+// to outer: sends read and write requests in CTC
+// from outer: receives read and write responses in CTC
+// to inner: sends read and write responses in TL
+class TileLinkToCTC(sinkIds: Int = 1, val beatBytes: Int = 8, baseAddr: BigInt = 0, size: BigInt = ((1L << 10) - 1), val maxBeats: Int = 4)
+                  (implicit p: Parameters) extends LazyModule {
+  val addrSet = AddressSet(baseAddr, size)
+  val node = TLManagerNode(Seq(TLSlavePortParameters.v1(
+    managers = Seq(TLSlaveParameters.v2(
+      address = Seq(addrSet),
+      regionType = RegionType.UNCACHED,
+      supports = TLMasterToSlaveTransferSizes(
+        putFull = TransferSizes(1, beatBytes*maxBeats), 
+        get = TransferSizes(1, beatBytes*maxBeats)
+      ),
+      fifoId = Some(0))), // requests are handled in order
+    beatBytes = beatBytes)))
+
+  lazy val module = new TileLinkToCTCModule(this)
+}
+
+class TileLinkToCTCModule(outer: TileLinkToCTC) extends LazyModuleImp(outer) {
+  val io = IO(new Bundle {
+    val flit = new DecoupledFlitIO(CTC.INNER_WIDTH)
+  })
+
+  val (mem, edge) = outer.node.in(0)
+  dontTouch(mem.d.bits.size)
+
+  // constants
+  val cmdLen = 2
+  val lenLen = 16
+  val headerLen = 64
+  val dataBits = mem.params.dataBits
+  val beatBytes = dataBits / 8
+  assert(beatBytes == outer.beatBytes, "Beat bytes mismatch")
+  val nChunksPerBeat = dataBits / CTC.INNER_WIDTH
+  val byteAddrBits = log2Ceil(beatBytes)
+  val nChunksPerHeader = headerLen / CTC.INNER_WIDTH
+
+  val len = Reg(UInt(lenLen.W))
+  val ctc_len = Reg(UInt(lenLen.W))
+  val a_lg_size = Reg(UInt(log2Ceil(beatBytes * outer.maxBeats).W))
+  val a_source = Reg(UInt(edge.bundle.sourceBits.W))
+  dontTouch(a_lg_size)
+  val cmd = Reg(UInt(cmdLen.W))
+  val addr = Reg(UInt(headerLen.W))
+  val tl_addr = Reg(UInt(headerLen.W))
+  val body = Reg(Vec(nChunksPerBeat, UInt(CTC.INNER_WIDTH.W)))
+
+  // ====== state machine ======
+  val (s_idle :: s_send_cmd :: s_send_addr :: s_recv_cmd:: s_recv_addr :: 
+    s_r_body :: s_r_ack :: s_w_req :: s_w_body :: s_w_ack ::s_reject :: Nil) = Enum(11)
+  val state = RegInit(s_idle)
+  val idx = Reg(UInt(log2Up(Math.max(nChunksPerBeat, nChunksPerHeader)).W))
+
+  // state-driven signals
+  io.flit.in.ready := state.isOneOf(s_r_body, s_recv_cmd, s_recv_addr)
+  io.flit.out.valid:= state.isOneOf(s_send_cmd, s_send_addr, s_w_body)
+  val out_bits = Mux(state === s_send_cmd, Cat(cmd, len - 1.U),
+    Mux(state === s_send_addr, addr(CTC.INNER_WIDTH - 1, 0), body(idx)))
+  io.flit.out.bits := out_bits.asTypeOf(io.flit.out.bits)
+
+  val is_single_word = ctc_len === 1.U
+  val shifted_body = Wire(UInt(dataBits.W))
+  shifted_body := Mux(is_single_word, body(0) << (tl_addr % beatBytes.U * 8.U), body.asUInt)
+  dontTouch(shifted_body)
+  val tl_write_ack = edge.AccessAck(a_source, a_lg_size)
+  val tl_read_ack = edge.AccessAck(a_source, a_lg_size, shifted_body)
+
+  mem.a.ready := state.isOneOf(s_idle, s_w_req)
+  mem.b.valid := false.B
+  mem.c.ready := false.B
+  mem.d.valid := state.isOneOf(s_r_ack, s_w_ack)
+  mem.d.bits := Mux(state === s_r_ack, tl_read_ack, tl_write_ack)
+  mem.e.ready := false.B
+
+
+  when (state === s_idle && mem.a.valid) {
+    len := (1.U << mem.a.bits.size) / CTC.INNER_WIDTH_BYTES.U
+    ctc_len := (1.U << mem.a.bits.size) / CTC.INNER_WIDTH_BYTES.U
+    a_lg_size := mem.a.bits.size
+    a_source := mem.a.bits.source
+    cmd := Mux(mem.a.bits.opcode === TLMessages.Get, CTCCommand.read_req, CTCCommand.write_req)
+    addr := mem.a.bits.address
+    tl_addr := mem.a.bits.address
+    state := Mux(mem.a.bits.size >= 2.U, s_send_cmd, s_reject)
+    // state := s_send_cmd
+    idx := 0.U
+    body := mem.a.bits.data.asTypeOf(body)
+    when (cmd === CTCCommand.write_req) { body := mem.a.bits.data.asTypeOf(body) }
+  }
+
+  when (state === s_send_cmd && io.flit.out.ready) {
+    state := s_send_addr
+  }
+
+  when (state === s_send_addr && io.flit.out.ready) {
+    addr := (addr >> CTC.INNER_WIDTH) 
+    idx := idx + 1.U
+    when (idx === (nChunksPerHeader - 1).U) {
+      idx := 0.U
+      state := Mux(cmd === CTCCommand.read_req, s_recv_cmd, s_w_body)
+    }
+  }
+
+  when (state === s_recv_cmd && io.flit.in.valid) {
+    assert((io.flit.in.bits.flit(lenLen - 1, 0) === ctc_len - 1.U), "Mismatch in CTC length")
+    val ack_cmd = Mux(cmd === CTCCommand.read_req, CTCCommand.read_ack, CTCCommand.write_ack)
+    assert(io.flit.in.bits.flit(cmdLen + lenLen - 1, lenLen) === ack_cmd, "Mismatch in CTC command")
+    state := s_recv_addr
+  } 
+
+  when (state === s_recv_addr && io.flit.in.valid) {
+    // assert(io.flit.in.bits.flit === ((addr >> (idx * CTC.INNER_WIDTH.U)) & ((1.U << CTC.INNER_WIDTH.U) - 1.U)), "Mismatch in CTC address")
+    when (idx === (nChunksPerHeader - 1).U) {
+      idx := 0.U
+      state := Mux(cmd === CTCCommand.read_req, s_r_body, s_w_ack)
+    } .otherwise {
+      idx := idx + 1.U
+    }
+  }
+
+  // BEGIN: handling read requests
+  // wait and collect the read response from CTC
+  when (state === s_r_body && io.flit.in.valid) {
+    body(idx) := io.flit.in.bits.flit.asUInt
+    idx := idx + 1.U
+    len := len - 1.U
+    when (idx === nChunksPerBeat.U - 1.U || len === 1.U) {
+      state := s_r_ack
+    }
+  }
+  // send the read acknoledgement to TL
+  when (state === s_r_ack && mem.d.ready) {
+    state := s_idle
+    when (len === 0.U) {
+      state := s_idle
+    } .otherwise {
+      idx := 0.U
+      state := s_r_body
+    }
+  }
+  // END: handling read requests
+
+  // BEGIN: handling write requests
+  // send the write data to CTC
+  when (state === s_w_body && io.flit.out.ready) {
+    idx := idx + 1.U
+    len := len - 1.U
+    when (idx === nChunksPerBeat.U - 1.U || len === 1.U) {
+      idx := 0.U
+      state := Mux(len === 1.U, s_recv_cmd, s_w_req)
+    }
+  }
+
+  // receive a new write beat from TL
+  when (state === s_w_req && mem.a.ready) {
+    state := s_w_body
+    body := mem.a.bits.data.asTypeOf(body)
+  }
+
+  // send the write acknoledgement to TL
+  when (state === s_w_ack && mem.d.ready) {
+    state := s_idle
+  }
+  // END: handling write requests
+
+  // BEGIN: reject sub-word transactions
+  when (state === s_reject && mem.d.ready) {
+    // TODO: send reject to TL D channel
+    state := s_idle
+  }
+  // END: reject sub-word transactions
+}
